93052a48b10eff2af1f571bb6344eda583ba24c0
maze <1309334291@qq.com>
2019-01-01 23:45:36 +0800

    c语言练习

---
+++ maze_0101/maze_0101/test.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+
+
+
+
+	//int a = -10;
+	//int *p = NULL;
+	//printf("%d\n", !2);
+	//printf("%d\n", !0);
+	//a = -a;
+	//p = &a;
+	//printf("%d\n", sizeof(a));
+	//printf("%d\n", sizeof(int));
+	//printf("%d\n", sizeof a);//这样写行不行？
+	//printf("%d\n", sizeof int);//这样写行不行？no
+
+
+
+	//int i = 1;
+	//int ret = (++i) + (++i) + (++i);
+	//printf("%d\n", i);    //4
+	//printf("%d\n", ret);  // 12
+	//char str[] = "hehe";	
+	//printf("%d\n", strlen(str));
+	//float a = 1;
+	//int b = 2;
+	//printf("%d", a / b);
+	
+	//int arr[4] = { 0,11,25,2, };
+	//int* p = arr + 1;
+	//printf("%d",p[-1]);
+
+	system("pause");
+	return 0;
+}
